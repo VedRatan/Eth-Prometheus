28f9725078dc919ef51e3676b783c95a84b9d213
Ved Ratan <vedratan8@gmail.com>
2023-02-18 22:33:25 +0530

    minute change

--- index.js
+++ index.js
@@ -8,10 +8,13 @@ const prometheus = require("./prometheus")
 const app = Express()
 const PORT = process.env.PORT || 5000
 
-app.use("/api", routes.APIRoutes)
-app.use(prometheus.middleware.handleLatency);
 
 prometheus.startMetricsServer(app)
+app.use(prometheus.middleware.handleLatency);
+app.use("/api", routes.APIRoutes)
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
